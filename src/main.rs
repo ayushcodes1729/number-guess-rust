42f58c8674f6c0251aec84cd145c1726d8663bea
Ayush Kumar <ayushtiwari0803@gmail.com>
2025-05-19 23:35:23 +0530

    Game V1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
-use std::io;
+use std::{cmp::Ordering, io};
+use rand::Rng;
 fn main(){
     println!("Number Guessing Game!!");
-    print!("Guess a number: ");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to  read line");
-
-    println!("You guessed {guess}");
+    let secret_number = rand::rng().random_range(1..=100);
+    println!("Secret number is {secret_number}");
+    loop {
+        print!("Guess a number: ");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to  read line");
+    
+        let guess: i32= guess.trim().parse().expect("Please type a number");
+        // In the above line rust allows us to shadow the previous value of guess with a new one. Shadowing lets us reuse the guess variable name rather than forcing us to create two unique variables, such as guess_str and guess.
+        println!("You guessed {guess}");
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too large"),
+            Ordering::Equal => {
+                println!("You guessed right");
+                break;
+            }
+        }
+    }
 }
