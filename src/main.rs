3c0d527bf13953c6081ac6e68bc6e224671b03a0
Ayush Kumar <ayushtiwari0803@gmail.com>
2025-05-19 23:47:36 +0530

    Error handling implemented

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,16 @@ fn main(){
     let secret_number = rand::rng().random_range(1..=100);
     println!("Secret number is {secret_number}");
     loop {
-        print!("Guess a number: ");
+        println!("Guess a number: ");
         let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Failed to  read line");
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to  read line");
     
-        let guess: i32= guess.trim().parse().expect("Please type a number");
+        let guess: u32= match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
         // In the above line rust allows us to shadow the previous value of guess with a new one. Shadowing lets us reuse the guess variable name rather than forcing us to create two unique variables, such as guess_str and guess.
         println!("You guessed {guess}");
     
